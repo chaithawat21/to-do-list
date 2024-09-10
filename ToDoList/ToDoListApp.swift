c7375faa008c414543076799dead49a6298e627b
chai <chaithawat.contact@gmail.com>
2024-09-10 10:49:23 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Chaithawat Pinsuwan on 10/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
